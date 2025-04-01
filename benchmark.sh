4e4dd757ec403113d724d9ad80bb8dc8dc40ff8b
Chris Twiner <chris.twiner@gmail.com>
2025-04-01 22:57:24 +0200

    wrong profiles

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "----> setting up source data"
+echo "----> setting up source data got params " "$@"
 mvn exec:java -Dexec.mainClass="com.sparkutils.quality_performance_tests.TestSourceData" -P "$@"
 echo "----> running benchmarks"
 mvn exec:java -Dexec.mainClass="com.sparkutils.quality_performance_tests.PerfTests" -P "$@"
