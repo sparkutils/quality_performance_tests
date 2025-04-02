846390898d6fec1c6eaff5cd10f1c52bc2fa8882
Chris Twiner <chris.twiner@gmail.com>
2025-04-02 08:08:07 +0200

    json tests

--- src/main/scala/com/sparkutils/quality_performance_tests/PerfTests.scala
+++ src/main/scala/com/sparkutils/quality_performance_tests/PerfTests.scala
@@ -21,6 +21,8 @@ object TestData {
     TestData("BR", "a", 1, 5, "ops"),
   )
 
+  val schema = "'location String, idPrefix String, id Int, page Long, department String'"
+
   def setup(rows: Int, sparkSession: SparkSession) = {
     import sparkSession.implicits._
 
@@ -29,29 +31,31 @@ object TestData {
         a <- 'a' to 'z'
         td <- dataBasis
       } yield td.copy(idPrefix = s"$a", id = id.toInt)).toSeq
-    }.selectExpr("explode(value) as f").selectExpr("f.*")
+    }.selectExpr("explode(value) as f").selectExpr("f.*","to_json(f) payload")
   }
 
   // fixed size of rules and complexity to start with (easier to compare with other tooling)
-  val ruleSuite = RuleSuite(Id(1,1),
+  val ruleSuite = rules("")
+
+  def rules(prefix: String) = RuleSuite(Id(1,1),
     Seq(
       RuleSet(Id(1,1),
         Seq(
-          Rule(Id(1,1), ExpressionRule("`dq is applicable for`(`Non UK`()) and department = 'sales'")),
-          Rule(Id(2,1), ExpressionRule("`dq is applicable for`(`UK & US`()) and department = 'marketing'")),
-          Rule(Id(3,1), ExpressionRule("`dq is applicable for`(`Americas`()) and department = 'hr'")),
-          Rule(Id(4,1), ExpressionRule("`dq is applicable for`(`Europe`()) and department = 'it'")),
-          Rule(Id(5,1), ExpressionRule("`isMissing`(id) and department = 'ops'")),
-          Rule(Id(6,1), ExpressionRule("`dq is applicable for`(`Non UK`()) and department = 'marketing'")),
-          Rule(Id(7,1), ExpressionRule("`dq is applicable for`(`UK & US`()) and department = 'hr'")),
-          Rule(Id(8,1), ExpressionRule("`dq is applicable for`(`Americas`()) and department = 'it'")),
-          Rule(Id(9,1), ExpressionRule("`dq is applicable for`(`Europe`()) and department = 'sales'")),
-          Rule(Id(10,1), ExpressionRule("`isMissing`(id) and department = 'sales'")),
-          Rule(Id(11,1), ExpressionRule("`dq is applicable for`(`Non UK`()) and department = 'it'")),
-          Rule(Id(12,1), ExpressionRule("`dq is applicable for`(`UK & US`()) and department = 'sales'")),
-          Rule(Id(13,1), ExpressionRule("`dq is applicable for`(`Americas`()) and department = 'marketing'")),
-          Rule(Id(14,1), ExpressionRule("`dq is applicable for`(`Europe`()) and department = 'hr'")),
-          Rule(Id(15,1), ExpressionRule("isMissing(id) and department = 'hr'")),
+          Rule(Id(1,1), ExpressionRule(s"`dq is applicable for`(`Non UK`()) and ${prefix}department = 'sales'")),
+          Rule(Id(2,1), ExpressionRule(s"`dq is applicable for`(`UK & US`()) and ${prefix}department = 'marketing'")),
+          Rule(Id(3,1), ExpressionRule(s"`dq is applicable for`(`Americas`()) and ${prefix}department = 'hr'")),
+          Rule(Id(4,1), ExpressionRule(s"`dq is applicable for`(`Europe`()) and ${prefix}department = 'it'")),
+          Rule(Id(5,1), ExpressionRule(s"`isMissing`(${prefix}id) and ${prefix}department = 'ops'")),
+          Rule(Id(6,1), ExpressionRule(s"`dq is applicable for`(`Non UK`()) and ${prefix}department = 'marketing'")),
+          Rule(Id(7,1), ExpressionRule(s"`dq is applicable for`(`UK & US`()) and ${prefix}department= 'hr'")),
+          Rule(Id(8,1), ExpressionRule(s"`dq is applicable for`(`Americas`()) and ${prefix}department = 'it'")),
+          Rule(Id(9,1), ExpressionRule(s"`dq is applicable for`(`Europe`()) and ${prefix}department = 'sales'")),
+          Rule(Id(10,1), ExpressionRule(s"`isMissing`(${prefix}id) and ${prefix}department = 'sales'")),
+          Rule(Id(11,1), ExpressionRule(s"`dq is applicable for`(`Non UK`()) and ${prefix}department = 'it'")),
+          Rule(Id(12,1), ExpressionRule(s"`dq is applicable for`(`UK & US`()) and ${prefix}department = 'sales'")),
+          Rule(Id(13,1), ExpressionRule(s"`dq is applicable for`(`Americas`()) and ${prefix}department = 'marketing'")),
+          Rule(Id(14,1), ExpressionRule(s"`dq is applicable for`(`Europe`()) and ${prefix}department = 'hr'")),
+          Rule(Id(15,1), ExpressionRule(s"isMissing(${prefix}id) and ${prefix}department = 'hr'")),
         )
       )
     ),
@@ -60,40 +64,50 @@ object TestData {
       LambdaFunction("UK & US", "array('US','UK')", Id(2,1)),
       LambdaFunction("Americas", "array('US','MX','BR')", Id(3,1)),
       LambdaFunction("Europe", "array('CH')", Id(4,1)),
-      LambdaFunction("dq is applicable for", "loc -> array_contains(loc, location)", Id(5,1)),
+      LambdaFunction(s"dq is applicable for", s"loc -> array_contains(loc, ${prefix}location)", Id(5,1)),
       LambdaFunction("isMissing", "wh -> if(wh is null, true, regexp_like(wh,'^\\s*$'))", Id(6,1))
     )
   )
 
-  val baseline = {
+  val jsonRuleSuite = {
+    val r = rules("jPayload().") // don't have to introduce a new function, but you would
+    r.copy(lambdaFunctions = r.lambdaFunctions :+ LambdaFunction("jPayload", s"from_json(payload, $schema)", Id(7,1)))
+  }
+
+
+  def baselineRules(prefix: String) = {
     import sparkSession.implicits._
 
     // the rules above do 16 rules ( 32 'tests' ), so simulating a struct with a bare bones, no lambdas,
-    // showing the typical overhead of Quality usage itself
+    // showing the typical overhead of Quality usage itself (result management, additional overhead of object creation etc.)
     sql.functions.struct(
-      sql.functions.expr("array_contains(array('US','CH','MX','BR'), location) and department = 'sales'"),
-      sql.functions.expr("array_contains(array('US','UK'), location) and department = 'marketing'"),
-      sql.functions.expr("array_contains(array('US','MX','BR'), location) and department = 'hr'"),
-      sql.functions.expr("array_contains(array('CH'), location) and department = 'it'"),
-      sql.functions.expr("if(id is null, true, regexp_like(id,'^\\s*$')) and department = 'ops'"),
-      sql.functions.expr("array_contains(array('US','CH','MX','BR'), location) and department = 'marketing'"),
-      sql.functions.expr("array_contains(array('US','UK'), location) and department = 'hr'"),
-      sql.functions.expr("array_contains(array('US','MX','BR'), location) and department = 'it'"),
-      sql.functions.expr("array_contains(array('CH'), location) and department = 'sales'"),
-      sql.functions.expr("if(id is null, true, regexp_like(id,'^\\s*$')) and department = 'sales'"),
-      sql.functions.expr("array_contains(array('US','CH','MX','BR'), location) and department = 'it'"),
-      sql.functions.expr("array_contains(array('US','UK'), location) and department = 'sales'"),
-      sql.functions.expr("array_contains(array('US','MX','BR'), location) and department = 'marketing'"),
-      sql.functions.expr("array_contains(array('CH'), location) and department = 'hr'"),
-      sql.functions.expr("if(id is null, true, regexp_like(id,'^\\s*$')) and department = 'hr'"),
+      sql.functions.expr(s"array_contains(array('US','CH','MX','BR'), ${prefix}location) and ${prefix}department = 'sales'"),
+      sql.functions.expr(s"array_contains(array('US','UK'), ${prefix}location) and ${prefix}department = 'marketing'"),
+      sql.functions.expr(s"array_contains(array('US','MX','BR'), ${prefix}location) and ${prefix}department = 'hr'"),
+      sql.functions.expr(s"array_contains(array('CH'), ${prefix}location) and ${prefix}department = 'it'"),
+      sql.functions.expr(s"if(${prefix}id is null, true, regexp_like(${prefix}id,'^\\s*$$')) and ${prefix}department = 'ops'"),
+      sql.functions.expr(s"array_contains(array('US','CH','MX','BR'), ${prefix}location) and ${prefix}department = 'marketing'"),
+      sql.functions.expr(s"array_contains(array('US','UK'), ${prefix}location) and ${prefix}department = 'hr'"),
+      sql.functions.expr(s"array_contains(array('US','MX','BR'), ${prefix}location) and ${prefix}department = 'it'"),
+      sql.functions.expr(s"array_contains(array('CH'), ${prefix}location) and ${prefix}department = 'sales'"),
+      sql.functions.expr(s"if(${prefix}id is null, true, regexp_like(${prefix}id,'^\\s*$$')) and ${prefix}department = 'sales'"),
+      sql.functions.expr(s"array_contains(array('US','CH','MX','BR'), ${prefix}location) and ${prefix}department = 'it'"),
+      sql.functions.expr(s"array_contains(array('US','UK'), ${prefix}location) and ${prefix}department = 'sales'"),
+      sql.functions.expr(s"array_contains(array('US','MX','BR'), ${prefix}location) and ${prefix}department = 'marketing'"),
+      sql.functions.expr(s"array_contains(array('CH'), ${prefix}location) and ${prefix}department = 'hr'"),
+      sql.functions.expr(s"if(${prefix}id is null, true, regexp_like(${prefix}id,'^\\s*$$')) and ${prefix}department = 'hr'"),
     )
   }
+
+  val baseline = baselineRules("")
+
+  val jsonBaseline = baselineRules(s"from_json(payload, $schema).")
 }
 
 object TestSourceData extends TestUtils {
   val inputsDir = "./target/testInputData"
 
-  val MAXSIZE = 1000000 // 10000000  10mil, takes about 1.5 - 2hrs on dev box
+  val MAXSIZE = 100000 // 10000000  10mil, takes about 1.5 - 2hrs on dev box
   val STEP =    100000
 
   def main(args: Array[String]): Unit = {
@@ -157,7 +171,20 @@ object PerfTests extends Bench.OfflineReport with TestUtils {
       forceInterpreted {
         using(rows) afterTests {sparkSession.close()} in evaluate(_.withColumn("quality", TestData.baseline), "baseline_interpreted")
       }
-    }/*
+    }
+
+    measure method "json baseline in codegen" in {
+      forceCodeGen {
+        using(rows) afterTests {sparkSession.close()} in evaluate(_.withColumn("quality", TestData.jsonBaseline), "json_baseline_codegen")
+      }
+    }
+
+    measure method "json baseline in interpreted" in {
+      forceInterpreted {
+        using(rows) afterTests {sparkSession.close()} in evaluate(_.withColumn("quality", TestData.jsonBaseline), "json_baseline_interpreted")
+      }
+    }
+    /*
 
    // the below aren't really that interesting, they perform well on lower row counts but not on higher counts
 
@@ -216,10 +243,39 @@ object PerfTests extends Bench.OfflineReport with TestUtils {
         }
       }
     }
+
     measure method "no forceEval in interpreted compile evals false - extra config" in {
       forceInterpreted {
         extraPerfOptions {
           using(rows) afterTests {sparkSession.close()} in evaluate(_.withColumn("quality", ruleRunner(TestData.ruleSuite, forceRunnerEval = false, compileEvals = false)), "no_forceEval_in_interpreted_compile_evals_false_extra_config")
+        }
+      }
+    }
+
+    measure method "json no forceEval in codegen compile evals false" in {
+      forceCodeGen {
+        using(rows) afterTests {sparkSession.close()} in evaluate(_.withColumn("quality", ruleRunner(TestData.jsonRuleSuite, forceRunnerEval = false, compileEvals = false)), "json_no_forceEval_in_codegen_compile_evals_false")
+      }
+    }
+
+    measure method "json no forceEval in interpreted compile evals false" in {
+      forceInterpreted {
+        using(rows) afterTests {sparkSession.close()} in evaluate(_.withColumn("quality", ruleRunner(TestData.jsonRuleSuite, forceRunnerEval = false, compileEvals = false)), "json_no_forceEval_in_interpreted_compile_evals_false")
+      }
+    }
+
+    measure method "json no forceEval in codegen compile evals false - extra config" in {
+      forceCodeGen {
+        extraPerfOptions {
+          using(rows) afterTests {sparkSession.close()} in evaluate(_.withColumn("quality", ruleRunner(TestData.jsonRuleSuite, forceRunnerEval = false, compileEvals = false)), "json_no_forceEval_in_codegen_compile_evals_false_extra_config")
+        }
+      }
+    }
+
+    measure method "json no forceEval in interpreted compile evals false - extra config" in {
+      forceInterpreted {
+        extraPerfOptions {
+          using(rows) afterTests {sparkSession.close()} in evaluate(_.withColumn("quality", ruleRunner(TestData.jsonRuleSuite, forceRunnerEval = false, compileEvals = false)), "json_no_forceEval_in_interpreted_compile_evals_false_extra_config")
         }
       }
     }
