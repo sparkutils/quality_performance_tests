d9a47cbe674c418584d22d61626b0074b5245e82
Chris Twiner <chris.twiner@gmail.com>
2025-04-10 10:52:18 +0200

    mimic audit cost

--- v0_1_3_1_RC4/src/main/scala/com/sparkutils/quality_performance_tests/PerfTestUtils.scala
+++ v0_1_3_1_RC4/src/main/scala/com/sparkutils/quality_performance_tests/PerfTestUtils.scala
@@ -11,7 +11,7 @@ object PerfTestUtils extends TestUtils {
       exec.maxWarmupRuns -> 4,
       exec.benchRuns -> 4,
       exec.jvmcmd -> (System.getProperty("java.home")+"/bin/java"),
-      exec.jvmflags -> List("-Xmx12g","-Xms12g") // 16GB on github runners
+      exec.jvmflags -> Args.args  // 16GB on github runners
       //  verbose -> true
     ) in {
 
