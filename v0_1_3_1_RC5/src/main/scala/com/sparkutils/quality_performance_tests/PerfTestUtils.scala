7d9c70e5acb708e6205c6e38c13613a305a9c99c
Chris Twiner <chris.twiner@gmail.com>
2025-04-15 19:10:00 +0200

    hardly any difference try adding fold due to cases

--- v0_1_3_1_RC5/src/main/scala/com/sparkutils/quality_performance_tests/PerfTestUtils.scala
+++ v0_1_3_1_RC5/src/main/scala/com/sparkutils/quality_performance_tests/PerfTestUtils.scala
@@ -3,19 +3,33 @@ import com.sparkutils.quality
 import com.sparkutils.quality.impl.extension.FunNRewrite
 import com.sparkutils.quality.ruleRunner
 import com.sparkutils.qualityTests.TestUtils
+import org.apache.spark.sql.catalyst.optimizer.ConstantFolding
 import org.scalameter.api._
 
 object PerfTestUtils extends TestUtils {
 
 
   val withRewrite = testPlan(FunNRewrite, secondRunWithoutPlan = false) _
+  val withExtraConstantFolding = testPlan(ConstantFolding, secondRunWithoutPlan = false) _
 
   /**
    * Enable any spark wide optimisations for a given run
    * @param thunk
    */
   def extraPerfOptions(thunk: Unit): Unit =
-    withRewrite(thunk)
+    withRewrite(
+      thunk
+    )
+  /**
+   * Enable any spark wide optimisations for a given run
+   * @param thunk
+   */
+  def rewriteAndFold(thunk: Unit): Unit =
+    withRewrite(
+      withExtraConstantFolding( // attempt a further constant fold for case statements
+        thunk
+      )
+    )
 
   trait ExtraPerfTests extends TestTypes.TheRunner with BaseConfig {
 
@@ -37,6 +51,15 @@ object PerfTestUtils extends TestUtils {
           }
         }
       }
+
+      measure method "no forceEval in codegen compile evals false - extra config fold" in {
+        _forceCodeGen {
+          rewriteAndFold {
+            using(rows) afterTests {sparkSession.close()} in evaluate(_.withColumn("quality", ruleRunner(TestData.ruleSuite, forceRunnerEval = false, compileEvals = false)), "no_forceEval_in_codegen_compile_evals_false_extra_config_fold")
+          }
+        }
+      }
+
       measure method "no forceEval in codegen compile evals false" in {
         _forceCodeGen {
             using(rows) afterTests {sparkSession.close()} in evaluate(_.withColumn("quality", ruleRunner(TestData.ruleSuite, forceRunnerEval = false, compileEvals = false)), "no_forceEval_in_codegen_compile_evals_false")
